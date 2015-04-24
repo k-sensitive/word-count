ce2f0c6d6fb63f730becc6e485742fdf9d001503
Kim Dang <kimdangg@gmail.com>
2015-04-24 09:32:08 -0700

    Add test to check a word in an entered phrase.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,8 @@ describe('String#word_count') do
   it('returns the number of times a word appears in a one word string') do
     expect("a".word_count("a")).to(eq(1))
   end
+
+  it('returns the number of times a word appears in a phrase') do
+    expect("a".word_count("a is a letter")).to(eq(2))
+  end
 end
