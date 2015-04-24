ce43511d3db6a2a79f7ebe10c4abbad57bf26df7
Kim Dang <kimdangg@gmail.com>
2015-04-24 09:46:59 -0700

    Add test for all caps.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,7 +6,11 @@ describe('String#word_count') do
     expect("a".word_count("a")).to(eq(1))
   end
 
-  it('returns the number of times a word appears in a phrase') do
+  it('returns the number of times a word appears in a string') do
     expect("a".word_count("a is a letter")).to(eq(2))
+  end
+
+  it('handles all uppercase entries in word and string input') do
+    expect("THY".word_count("THY is thy")).to(eq(2))
   end
 end
